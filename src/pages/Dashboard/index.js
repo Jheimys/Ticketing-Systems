3dfce3ba931c191b5130fbf4174c376733f2aec3
Jheimys Bassani <jheimysbb@gmail.com>
2024-02-09 08:39:49 +0000

    :construction: Creating private routes

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,20 @@
+import { useContext } from "react"
+import { AuthContext } from "../../contexts/auth"
 
 
 const Dasboard = () => {
+  const {logout} = useContext(AuthContext)
+
+  async function handleLogout(){
+    await logout()
+  }
+
   return (
-    <div>Dasboard</div>
+    <div>
+      <h1>Dasboard</h1>
+      <button onClick={handleLogout}>Sair da conta</button>
+
+    </div>
   )
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,14 @@ import {Routes, Route} from 'react-router-dom'
 import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp'
 import Dasboard from '../pages/Dashboard'
+import Private from './Private'
 
 const RoutesApp = () => {
   return (
     <Routes>
       <Route path='/' element={ <SignIn /> } />
       <Route path='/register' element={ <SignUp /> } />
-      <Route path='/dashboard' element={ <Dasboard /> }/>
+      <Route path='/dashboard' element={<Private> <Dasboard /> </Private> }/>
     </Routes>
   )
 }
