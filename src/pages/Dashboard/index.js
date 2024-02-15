72126ec975dbfea4332aab5d5773a3d698b480eb
Jheimys Bassani <jheimysbb@gmail.com>
2024-02-15 08:48:14 +0000

    :construction: Create the dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,18 @@
 import { useContext } from "react"
+
+//--Contexts
 import { AuthContext } from "../../contexts/auth"
+
+//--Components
 import  Header  from "../../components/Header"
+import Title from "../../components/Title"
+
+//-- React Icons
+import { FiEdit2, FiMessageSquare, FiPlus, FiSearch } from "react-icons/fi"
+import { Link } from "react-router-dom"
+
+//--style
+import './dasboard.css'
 
 
 const Dasboard = () => {
@@ -13,8 +25,47 @@ const Dasboard = () => {
   return (
     <div>
       <Header />
-      <h1>Dasboard</h1>
-      <button onClick={handleLogout}>Sair da conta</button>
+      
+      <div className="content">
+        <Title name="Tickets">
+          <FiMessageSquare size={25} />
+        </Title>
+
+        <>
+          <Link to="/new" className="new">
+            <FiPlus size={25} color="#FFF"/>
+            Novo chamado
+          </Link>
+
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Cliente</th>
+                <th scope="col">Assuntos</th>
+                <th scope="col">Status</th>
+                <th scope="col">Cadastrando em</th>
+                <th scope="col">#</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td data-label='Cliente'>Mercado da esquina</td>
+                <td data-label='Assuntos'>Suporte</td>
+                <td data-label='Status'>Em aberto</td>
+                <td data-label='Cadastrado'>12/05/2022</td>
+                <td data-label='#'>
+                  <button className="action" style={{backgroundColor: '#3583f6'}}>
+                    <FiSearch color="#FFF" size={17}/>
+                  </button>
+                  <button className="action" style={{backgroundColor: '#f6a935'}}>
+                    <FiEdit2 color="#FFF" size={17}/>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      </div>
 
     </div>
   )
